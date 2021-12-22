2921f0560990e2306dafe0309630733730c15b74
Aleksey Popov <alopen@yandex.ru>
2021-12-22 13:39:42 +0700

    Restarting VPN container on the same image.

--- vpn_poc.rb
+++ vpn_poc.rb
@@ -15,23 +15,36 @@ class VpnPoc
     raise 'DO client is not initialized. Please pass either "client" or "token"' if @client.nil?
   end
 
+  def stop_existing_containers(ssh)
+    @logger.debug("Checking existing containers")
+    docker_ps_output = ssh.exec!("docker ps -q")
+    container_ids = docker_ps_output.split(/\s+/)
+    @logger.debug("Container IDs: #{container_ids}")
+    container_ids.each do |container_id|
+      @logger.debug("Stopping container #{container_id}")
+      ssh.exec!("docker stop #{container_id}")
+    end
+  end
+
   def do_its_best
-    start_time = Time.new
     droplet_id = create_or_find_droplet
     droplet_by_id = @client.droplets.find(id: droplet_id)
     ip_address = droplet_by_id.networks['v4'].select {|net| net.type == 'public'}.first.ip_address
     Net::SSH.start(ip_address, 'root', keys: @keys, timeout: 300) do |ssh|
-      @logger.debug('Connected to droplet')
-      cid = ssh.exec!('docker run -d --restart=always --privileged -p 1194:1194/udp -p 80:8080/tcp -e HOST_ADDR=$(curl -s https://api.ipify.org) alekslitvinenk/openvpn 2>/dev/null')
+      @logger.info('Connected to droplet')
+      self.stop_existing_containers(ssh)
+      @logger.debug('Launching new container')
+      cid = ssh.exec!('docker run -d --restart=always --privileged -p 1194:1194/udp -p 8088:8080/tcp -e HOST_ADDR=$(curl -s https://api.ipify.org) alekslitvinenk/openvpn 2>/dev/null')
       cid.chop!
       @logger.debug("VPN container ID: #{cid}")
       @logger.debug 'Waiting 2 minutes for container to start'
       sleep 120
-      `curl -o vpn.ovpn http://#{ip_address}`
     end
-    Net::SFTP.start(ip_address, 'root', keys: @keys) do |sftp|
-      sftp.download('/root/client.ovpn', 'vpn.ovpn')
-    end
+    `curl -o vpn.ovpn http://#{ip_address}:8088`
+    # maybe docker cp #{cid}:/opt/Dockovpn/config/client.ovpn client.conf
+    # Net::SFTP.start(ip_address, 'root', keys: @keys) do |sftp|
+    #   sftp.download('/root/vpn.ovpn', 'vpn.ovpn')
+    # end
     `open vpn.ovpn`
   end
 
@@ -52,5 +65,5 @@ class VpnPoc
     droplet_create_result.id
   end
 end
-token = File.open('do.token').readline().chomp()
+token = File.open('do.token').readline.chomp
 VpnPoc.new(token: token).do_its_best
